347a0037655fe03e957258fbdc138a79beeacba9
Paul <greenhan@hotmail.com>
2019-01-30 22:45:09 -0800

    First Commit

---
+++ src/paul_test1/test1.java
@@ -0,0 +1,10 @@
+package paul_test1;
+
+public class test1 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("hello");
+	}
+
+}
---
+++ src/test2/BasicSeleniumCommands.java
@@ -0,0 +1,15 @@
+package test2;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BasicSeleniumCommands {
+
+	public static void main(String[] args) {
+		
+		WebDriver driver = new ChromeDriver();
+		
+
+	}
+
+}
